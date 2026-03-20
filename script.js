3f6b5407e16d91824d95c758ad0209d618332269
skywarspro15 <potatobg648@gmail.com>
2026-03-20 18:38:35 +0800

    WiP landing page

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.15,
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  const animatedElements = document.querySelectorAll(".scroll-anim");
+  animatedElements.forEach((el) => observer.observe(el));
+});
